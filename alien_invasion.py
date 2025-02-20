36e641cad2b96616dae8bb4cfbce3fe3c723a4a6
AneliBem <tolya.bem@gmail.com>
2025-02-20 15:54:13 +0200

    create setting.py (setting window and bg_color window). Connect setting.py to alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,17 @@
 import sys 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Клас для управління ресурсами і поведінкою гри"""
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.bg_color = (230, 230, 230)
@@ -18,7 +22,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
         
