53e3b6b26885d55d3b0c5a8596b0ee67bd75d65d
AneliBem <tolya.bem@gmail.com>
2025-02-21 15:10:28 +0200

    add moving ship in game

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,15 +18,27 @@ class AlienInvasion:
         self.ship = Ship(self)
    
     def run_game(self):
-        while True:  
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        while True:
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self): 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
+            
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.rect.x += -1
 
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
         
 if __name__ == '__main__':
     ai = AlienInvasion()
