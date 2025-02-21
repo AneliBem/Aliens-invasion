68f151b4ff8e181ef62892623499425032848282
AneliBem <tolya.bem@gmail.com>
2025-02-21 15:58:49 +0200

    pressing keyboard for moving ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
 
     def _check_events(self): 
         for event in pygame.event.get():
@@ -28,12 +29,16 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
-            
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    self.ship.rect.x += -1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
 
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
