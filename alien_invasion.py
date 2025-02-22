667897fb2be4548cf4766ed8f6650fb784849c11
AneliBem <tolya.bem@gmail.com>
2025-02-22 16:22:13 +0200

    add gun in ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame # pip/pip3 install pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Class for manage resources and behaviour game"""
@@ -24,10 +25,12 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
    
     def run_game(self):
         while True:
             self._check_events()
+            self.bullets.update()
             self._update_screen()
             self.ship.update()
 
@@ -45,19 +48,26 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit
+        elif event.key == pygame.K_q: 
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-                
+    
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
         
 if __name__ == '__main__':
