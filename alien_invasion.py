364bbd0ef9a650d50eec566b6ccce14ce2beb57a
AneliBem <tolya.bem@gmail.com>
2025-02-27 17:03:50 +0200

    add regeneration group aliens and changed speed bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,6 +50,13 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
 
     def _check_events(self): 
         for event in pygame.event.get():
